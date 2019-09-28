19d1db0356f1c75a8006ff45f1b378773618792a
m-ling <2399144697@qq.com>
2019-09-28 23:37:40 +0800

    the_4_2019/9/28

--- js/main.js
+++ js/main.js
@@ -2,5 +2,12 @@ $(window).scroll(function(){
     var scrollH=$(document).scrollTop();
     var viewH = $(window).height();  
     console.log(scrollH);//380
+    if(scrollH>380){
+         $('#nav').css('height','86px')
+        //  $('.img_head').css('background-attachment','scroll')
+    }else{
+        $('#nav').css('height','0')
+    }
+    
 })
 
