7009b8815aaf08cbb28662add980376da4e6307b
m-ling <2399144697@.com>
2019-09-30 16:57:13 +0800

    the_5_2019/9/30

--- js/main.js
+++ js/main.js
@@ -3,10 +3,12 @@ $(window).scroll(function(){
     var viewH = $(window).height();  
     console.log(scrollH);//380
     if(scrollH>380){
-         $('#nav').css('height','86px')
+         $('#nav').css('height','86px');
         //  $('.img_head').css('background-attachment','scroll')
+        $('.menus').css('display','none');
     }else{
-        $('#nav').css('height','0')
+        $('#nav').css('height','0');
+        $('.menus').css('display','block');
     }
     
 })
