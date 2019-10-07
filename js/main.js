88dc4aaf4de148391caf9b210860bd7d54f4dce9
m-ling <2399144697@qq.com>
2019-10-07 22:18:52 +0800

    the_6_2019/10/7

--- js/main.js
+++ js/main.js
@@ -1,15 +1,135 @@
+// 滚动条
+
+$(document).ready(
+    function() { 
+      $("html").niceScroll({
+        cursorcolor: "#f6a16a", // 改变滚动条颜色，使用16进制颜色值
+        cursoropacitymin: .5, // 当滚动条是隐藏状态时改变透明度, 值范围 1 到 0
+        cursoropacitymax: 1, // 当滚动条是显示状态时改变透明度, 值范围 1 到 0
+        cursorwidth: "7px", // 滚动条的宽度，单位：便素
+        cursorborder: "1px solid #fff", // CSS方式定义滚动条边框
+        cursorborderradius: "5px", // 滚动条圆角（像素）
+        autohidemode: true, // 隐藏滚动条的方式, 可用的值: 
+        background: "#f9eae4", // 轨道的背景颜色
+        iframeautoresize: true, // 在加载事件时自动重置iframe大小
+        cursorminheight: 32, // 设置滚动条的最小高度 (像素)
+        spacebarenabled: true, // 当按下空格时使页面向下滚动
+      });
+    }
+  );
+  $(document).ready(
+    function() { 
+      $(".ter_content").niceScroll({
+        cursorcolor: "#ccc", // 改变滚动条颜色，使用16进制颜色值
+        cursoropacitymin: 0, // 当滚动条是隐藏状态时改变透明度, 值范围 1 到 0
+        cursoropacitymax: 1, // 当滚动条是显示状态时改变透明度, 值范围 1 到 0
+        cursorwidth: "2px", // 滚动条的宽度，单位：便素
+        cursorborder: "1px solid #fff", // CSS方式定义滚动条边框
+        cursorborderradius: "5px", // 滚动条圆角（像素）
+        autohidemode: true, // 隐藏滚动条的方式, 可用的值: 
+        background: "#424242", // 轨道的背景颜色
+        iframeautoresize: true, // 在加载事件时自动重置iframe大小
+        cursorminheight: 32, // 设置滚动条的最小高度 (像素)
+        spacebarenabled: true, // 当按下空格时使页面向下滚动
+      });
+    }
+  );
+
+  $(document)
+  .on('click', 'a[href*="#"]', function() {
+    if ( this.hash ) {
+      $.bbq.pushState( '#/' + this.hash.slice(1) );
+      return false;
+    }
+  })
+  .ready(function() {
+    $(window).bind('hashchange', function(event) {
+      var tgt = location.hash.replace(/^#\/?/,'');
+      if ( document.getElementById(tgt) ) {
+        $.smoothScroll({scrollTarget: '#' + tgt});
+      }
+    });
+
+    $(window).trigger('hashchange');
+  });
+// 标题栏 
 $(window).scroll(function(){
     var scrollH=$(document).scrollTop();
     var viewH = $(window).height();  
-    console.log(scrollH);//380
     if(scrollH>380){
-         $('#nav').css('height','86px');
+         $('#nav').addClass('nav_tit').removeClass('mm')
         //  $('.img_head').css('background-attachment','scroll')
-        $('.menus').css('display','none');
+        $('.menus_icon').css('display','none');
     }else{
-        $('#nav').css('height','0');
-        $('.menus').css('display','block');
+        $('#nav').removeClass('nav_tit').addClass('mm').css('height','0');
+        $('.menus_icon').show();
     }
     
 })
+//下拉菜单
+window.onload = function () {
+// var menus=document.getElementById("menus");
+// menus.onclick=function(){
+//   console.log(123);
+// }
+$('#menus').click(function(){
+       var box=$('.menus_box')
+       box.toggleClass('menus_chg');
+       $('.menus_icon').toggleClass('menus_icon_2').css('margin-top','0');
+       $('.menus_detail>ul').toggle();
+})
 
+// 
+var i=0;//计数
+var img_wid=152;//每个li的固定宽度
+var img_tim=500;//每次轮播动画持续的时间
+var img_count=10;
+
+var ulImgs=$('.ul_imgs');
+function moveTo(){  
+ if(i==0){
+  ulImgs.addClass('transition');
+ }i++;
+ ulImgs.css('margin-left',`${-i*img_wid}px`);
+ if(i==img_count){
+   i=0;
+   setTimeout(function(){
+     ulImgs.removeClass('transition');//定时清除过渡
+     ulImgs.css('margin-left','0');
+   }, img_tim)
+ }
+}
+setInterval(()=>{
+ moveTo();
+},2000);
+
+// 
+$('#btn_1').click(function(){
+  console.log(123);
+  $(this).css({
+    background:'#000',
+    border:'0',
+  })
+  $('#btn_2').css({
+    background:'#2d2f34',
+    border:'1px solid #ddd',
+  })
+  $('.info-list1').show();
+  $('.info-list2').hide();
+})
+$('#btn_2').click(function(){
+  console.log(123);
+  $('#btn_1').css({
+    background:'#2d2f34',
+    border:'1px solid #ddd',
+  })
+  $(this).css({
+    background:'#000',
+    border:'0',
+  })
+  $('.info-list2').show();
+  $('.info-list1').hide();
+})
+
+
+}
